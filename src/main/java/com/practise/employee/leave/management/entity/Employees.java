f308e9c23632076a477ac81dad1bc1448de9991d
kaushik <kaushikcg15@gmail.com>
2025-09-02 13:02:34 +0530

    Employeees file commit

--- src/main/java/com/practise/employee/leave/management/entity/Employees.java
+++ src/main/java/com/practise/employee/leave/management/entity/Employees.java
@@ -1,23 +1,30 @@
 package com.practise.employee.leave.management.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "employees")
 public class Employees {
 	
 	@Id
+	@Column(name="emp_id")
 	private String empId;
+	@Column(name = "emp_name")
 	private String empName;
+	private String department;
 	
 	public Employees() {
 		super();
 	}
 
-	public Employees(String empId, String empName) {
+	public Employees(String empId, String empName, String department) {
 		super();
 		this.empId = empId;
 		this.empName = empName;
+		this.department = department;
 	}
 
 	public String getEmpId() {
@@ -35,7 +42,15 @@ public class Employees {
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
-	
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
 	
 
 }
